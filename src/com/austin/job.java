df5fd35b92b3d138c36bd886fea353fea3a12e68
Austin White <whitea17@spu.edu>
2022-03-20 21:23:41 -0700

    Added to string method to job class
    Will use this in main later

--- src/com/austin/job.java
+++ src/com/austin/job.java
@@ -1,10 +1,12 @@
 package com.austin;
 
-public class job {
+import java.util.Comparator;
+
+public class job implements Comparable<job> {
     private int weight = 1;     // Value of job
     private int start  = 0;     // Start time
     private int end    = 0;     // End time
-    private int nextBestJob = 0;
+    private int nextBestJob = -1;
 
     job(int w, int s, int e){
         this.weight = w;
@@ -29,4 +31,17 @@ public class job {
 
     // setter
     public void setNextBestJob(int next){nextBestJob = next;}
+
+    public String toString(){
+        return "Start: " + start + ", End: " + end + ", Weight: " + weight;
+    }
+
+    @Override
+    public int compareTo(job jobToCompare){
+        int thisJob = this.end;
+        int otherJob = jobToCompare.getEnd();
+
+        return thisJob - otherJob;
+    }
+
 }
